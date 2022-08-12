b01c2dcf8f7eccdfea9434eda988602324a0f332
Amereen Hanvel <amereenhanvel786@gmail.com>
2022-08-12 14:13:28 -0400

    commit tree 395f8b983edeca5f7388cb1d59f22c3b5a2f7ab1

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
+let num1 = 1
+  let num2 = 62
+    const multiply = num1 * num2      
+      console.log (num1 * num2)
 
-// Write your code here
+
+
+let random = Math.floor(Math.random() * 1000) ;
+console.log(random)
+
+
+let num3 = 19
+ let num4 = 5
+    let mod = num3 % num4 
+      console.log(19 % 5)
+
+
+    let max= 20
+      Math.max (5, 10, 15, 20) 
